566ab959de2445ef4fb5b9410bb1dabfe4fe665b
ibrahim Asik <asikibrahim93@gmail.com>
2015-05-08 23:38:35 +0300

    Yeni hali eklendi

---
+++ TKYS_v1.1/App_Code/VeriErisim/GelirVeritabani.cs
@@ -0,0 +1,23 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GelirVeritabani
+/// </summary>
+public class GelirVeritabani : Veritabani
+{
+	public void gelir()
+    {
+        var yeni = new gelir
+        {
+            ad = "test",
+            miktar = 100,
+        };
+
+        Entity.gelir.Add(yeni);
+        Entity.SaveChanges();
+    }
+}
---
+++ TKYS_v1.1/App_Code/VeriErisim/Veritabani.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Entity;
+
+/// <summary>
+/// Summary description for Veritabani
+/// </summary>
+public class Veritabani
+{
+    protected TKYSEntities _entity;
+    protected TKYSEntities Entity
+    {
+        get
+        {
+            if (_entity == null)
+                _entity = new TKYSEntities();
+            return _entity;
+        }
+    }
+}
