bfd1b72f8a2168746969f9d58b1a6ba8eddde811
taylanicgen <taylanicgen@gmail.com>
2015-05-08 18:40:15 +0300

    class oluşturma

---
+++ tkys.v1.0/App_Code/veri_erisim/veritabani.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for veritabani
+/// </summary>
+public class veritabani
+{
+
+   
+    protected TKYSEntities _entity;
+    protected TKYSEntities Entity
+    {
+        get
+        {
+            if (_entity == null)
+                _entity = new TKYSEntities();
+            return _entity;
+        }
+    }
+}
