3bc72621007a519aa3eccf4769492e8a71bc5582
ibrahim Asik <asikibrahim93@gmail.com>
2015-05-09 22:08:05 +0300

    test işlemi yapıldı

--- TKYS_v1.1/backend/Default.aspx.cs
+++ TKYS_v1.1/backend/Default.aspx.cs
@@ -13,22 +13,6 @@ public partial class backend_Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        UyeVeritabani uye = new UyeVeritabani();
-        kullanici k1 = new kullanici();
-        k1.ad = ad.Text;
-        k1.soyad = soyad.Text;
-        k1.cinsiyet = true;
-        k1.cep_telefonu =cep_tel.Text;
-        k1.e_posta= posta.Text;
-        k1.kullanici_adi = kullanici.Text;
-        k1.sifre = sifre.Text;
-        k1.yetki = "1";
-
-
-        var islem = uye.UyeOl(k1);
-        if (islem.BasariliMi)
-            Label1.Text = "Good job boyy.. :D";
-        else
-            Label1.Text = "olmadı be .s";
+       
     }
 }
