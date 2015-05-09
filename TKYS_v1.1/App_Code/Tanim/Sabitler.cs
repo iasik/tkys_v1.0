11a99134e309458f77d9b877440133ca7cb5e952
taylanicgen <taylanicgen@gmail.com>
2015-05-09 16:52:48 +0300

    uye paneli

---
+++ TKYS_v1.1/App_Code/Tanim/Sabitler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Sabitler
+/// </summary>
+public enum UyeTip
+{
+
+    yonetici=1,
+    personel=2,
+    site_sakini=3,
+}
--- TKYS_v1.1/backend/backend.master.cs
+++ TKYS_v1.1/backend/backend.master.cs
@@ -9,6 +9,21 @@ public partial class backend_backend : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        YetkiKontrol();
+    }
 
+    public void YetkiKontrol()
+    {
+        if(Session[SiteTanim.KullaniciSession]!=null)
+        {
+            if (((kullanici_bilgisi)Session[SiteTanim.KullaniciSession]).yetki == UyeTip.yonetici || ((kullanici_bilgisi)Session[SiteTanim.KullaniciSession]).yetki == UyeTip.personel)
+            {
+                kullanici_adi.Text = ((kullanici_bilgisi)Session[SiteTanim.KullaniciSession]).kullanici_adi;
+            }
+            else
+                Response.Redirect("../default.aspx");
+        }
+        else
+            Response.Redirect("../default.aspx");
     }
 }
