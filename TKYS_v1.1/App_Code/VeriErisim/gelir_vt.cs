1b26b09187d9964c5d9b250467e19253ebcbf51a
taylanicgen <taylanicgen@gmail.com>
2015-05-09 18:59:22 +0300

    Gelir Tablosu Sil Listele

--- TKYS_v1.1/App_Code/VeriErisim/gelir_vt.cs
+++ TKYS_v1.1/App_Code/VeriErisim/gelir_vt.cs
@@ -107,6 +107,76 @@ public class gelir_vt : Veritabani
         }
     }
 
+    public islem_sonucu<bool> Sil(int id)
+    {
+        try
+        {
+            var kayitlar = (from g in Entity.gelir
+                            where g.id == id
+                            select g);
+            if(kayitlar.Count()>0)
+            {
+                var kayit = kayitlar.FirstOrDefault();
+
+                Entity.gelir.Remove(kayit);
+                Entity.SaveChanges();
+                return new islem_sonucu<bool>
+                {
+                    basarili_mi = true
+                };
+            }
+            else
+            {
+                return new islem_sonucu<bool>
+                {
+                    basarili_mi = false,
+                    mesaj = "veri bulunamadı",
+                };
+            }
+        }
+        catch (Exception hata)
+        {
+            return new islem_sonucu<bool>
+            {
+                basarili_mi = false,
+                hata_bilgi = new hata
+                {
+                    metod = "Sil",
+                    sinif = "gelir veritabani",
+                    hata_mesaji = hata.Message,
+                },
+            };
+        }
+    }
+
+    public islem_sonucu<List<gelir>> Listele()
+    {
+        try
+        {
+            var kayitlar = (from g in Entity.gelir
+                            select g).ToList();
+
+            return new islem_sonucu<List<gelir>>
+            {
+                basarili_mi = true,
+                veri = kayitlar,
+            };
+        }
+        catch (Exception hata)
+        {
+            return new islem_sonucu<List<gelir>>
+            {
+                basarili_mi = false,
+                hata_bilgi = new hata
+                {
+                    metod = "Listele",
+                    sinif = "gelir veritabani",
+                    hata_mesaji = hata.Message,
+                },
+            };
+        }
+        
+    }
     public void test()
     {
         var kayit = (from g in Entity.gelir
---
+++ TKYS_v1.1/backend/gelir/listele.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/backend/backend.master" AutoEventWireup="true" CodeFile="listele.aspx.cs" Inherits="backend_gelir_listele" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="main" Runat="Server">
+    <table class="table table-striped table-hover" id="sample_5">
+        <thead>
+            <tr>
+                <th>id
+                </th>
+                <th>Ad
+                </th>
+                <th class="hidden-xs">Miktar
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <asp:Repeater ID="rpt_gelir" runat="server" ItemType="Entity.gelir">
+                <ItemTemplate>
+                    <tr>
+                        <td><%#Item.id %>
+                        </td>
+                        <td><%#Item.ad %>
+                        </td>
+                        <td><%#Item.miktar %>
+                        </td>
+                        <td>
+                            <a href="guncelle.aspx?id=<%#Item.id %>">Güncelle</a>
+                            <a href="sil.aspx?id=<%#Item.id %>">Sil</a>
+                        </td> 
+                    </tr>
+                </ItemTemplate>
+            </asp:Repeater>
+        </tbody>
+    </table>
+</asp:Content>
+
