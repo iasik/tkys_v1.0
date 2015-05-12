52e35544ddc4d8eddef3daa42c109e47aab4969e
ibrahim Asik <asikibrahim93@gmail.com>
2015-05-12 15:07:30 +0300

    default aspxler düzenlendi

--- TKYS_v1.1/backend/genel/Default.aspx
+++ TKYS_v1.1/backend/genel/Default.aspx
@@ -4,8 +4,6 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="main" runat="Server">
     <form id="form1" runat="server">
-    
-        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
 
     </form>
 </asp:Content>
--- TKYS_v1.1/backend/genel/Default.aspx.cs
+++ TKYS_v1.1/backend/genel/Default.aspx.cs
@@ -10,10 +10,6 @@ public partial class backend_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //TKYSEntities entity = new TKYSEntities();
-        //var kayitlar =(from u in entity.kullanici select u).ToList();
-        //GridView1.DataSource = kayitlar;
-        //GridView1.DataBind();
 
     }
 }
