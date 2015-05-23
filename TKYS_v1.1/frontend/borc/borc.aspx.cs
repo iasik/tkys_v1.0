10d734bc1f74f00da7898fb7e75c9b7f3a8241e7
ibrahim Asik <asikibrahim93@gmail.com>
2015-05-23 17:55:32 +0300

    eksik kalan veriler eklendi

---
+++ TKYS_v1.1/frontend/borc/borc.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class frontend_borc_borc : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        borc();
+    }
+
+    protected void borc()
+    {
+        borc_vt borc = new borc_vt();
+        var sonuc = borc.kullanici_borc(((kullanici_bilgisi)Session[SiteTanim.KullaniciSession]).id);
+
+        if(sonuc.basarili_mi)
+        {
+            rpt_borc.DataSource = sonuc.veri;
+            rpt_borc.DataBind();
+        }
+    }
+}
