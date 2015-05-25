9b1afb0486be76813e347b9981ce6fe303350b12
keremkaya <keremkayaa@gmail.com>
2015-05-25 23:38:10 +0300

    rezervasyonnnn

---
+++ TKYS_v1.1/frontend/rezervasyon/rezervasyonlarim.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class frontend_rezervasyon_rezervasyonlarim : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Rezervasyonlarim();
+    }
+
+    public void Rezervasyonlarim()
+    {
+        rezervasyon_vt rezervasyon = new rezervasyon_vt();
+        var sonuc = rezervasyon.ListeleID(((kullanici_bilgisi)Session[SiteTanim.KullaniciSession]).id);
+        if (sonuc.basarili_mi)
+        {
+            rpt_Rezervasyonum.DataSource = sonuc.veri;
+            rpt_Rezervasyonum.DataBind();
+        }
+        else
+        {
+
+        }
+
+
+    }
+}
